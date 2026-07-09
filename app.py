87bbb7c96dba255453fd42d89fe4d531d813e6c1
Bien Nguyen Cong <bien.nguyen.cong@beetechjsc.com>
2026-07-09 16:57:10 +0700

    ci: replace CodePipeline with GitHub Actions (OIDC-based deploy)

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import aws_cdk as cdk
 from todo_api.todo_api_stack import TodoApiStack
-from todo_api.pipeline_stack import PipelineStack
+# from todo_api.pipeline_stack import PipelineStack
 
 app = cdk.App()
 
@@ -14,13 +14,13 @@ TodoApiStack(
     ),
 )
 
-PipelineStack(
-    app,
-    "PipelineStack",
-    env=cdk.Environment(
-        account="772908588074",
-        region="ap-southeast-1",
-    ),
-)
+# PipelineStack(
+#     app,
+#     "PipelineStack",
+#     env=cdk.Environment(
+#         account="772908588074",
+#         region="ap-southeast-1",
+#     ),
+# )
 
 app.synth()
